08debc5d6c3372fbc4b193e7a66a7424fd6a6574
manjunath-satyamurthy <pass2rahul@gmail.com>
2017-07-27 16:34:39 +0200

    Removed Unnecessary files, Installed Webpack

---
+++ app/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      }
+    ]
+  },
+}
